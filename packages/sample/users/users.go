c56bf56839e9320cd2ee0b8eea8eff8ff00a1ea8
shahar-levy <slevy@digitalocean.com>
2022-06-06 10:37:16 -0700

    testing

--- packages/sample/users/users.go
+++ packages/sample/users/users.go
@@ -1,73 +1,67 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-type Response struct {
-	StatusCode int               `json:"statusCode,omitempty"`
-	Headers    map[string]string `json:"headers,omitempty"`
-	Body       string            `json:"body,omitempty"`
-}
-
 func Main() {
 
 	r := mux.NewRouter()
-	// r.HandleFunc("/sample/users/user/{id}", getUser).Methods("GET")
-	// r.HandleFunc("/sample/users/user/{id}/score/{score}", postUser).Methods("POST")
-	// r.HandleFunc("/sample/users/user/{id}", deleteUser).Methods("DELETE")
+	r.HandleFunc("user/{id}", getUser).Methods("GET")
+	r.HandleFunc("user/{id}/score/{score}", postUser).Methods("POST")
+	r.HandleFunc("users/user/{id}", deleteUser).Methods("DELETE")
 
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		path := r.URL.Path
-		w.Write([]byte("function is working on path " + path))
+	r.HandleFunc("/testing", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("function is working"))
 	})
 
 }
 
-// type User struct {
-// 	Id    string
-// 	Score int
-// }
+type User struct {
+	Id    string
+	Score int
+}
 
-// // /user/{id}
-// func getUser(w http.ResponseWriter, r *http.Request) {
+// /user/{id}
+func getUser(w http.ResponseWriter, r *http.Request) {
 
-// 	params := mux.Vars(r)
-// 	userid := params["id"]
+	params := mux.Vars(r)
+	userid := params["id"]
 
-// 	// fake user
-// 	testUser := User{
-// 		Id:    userid,
-// 		Score: 10,
-// 	}
+	// fake user
+	testUser := User{
+		Id:    userid,
+		Score: 10,
+	}
 
-// 	userJSON, err := json.Marshal(testUser)
-// 	if err != nil {
-// 		panic(err)
-// 	}
+	userJSON, err := json.Marshal(testUser)
+	if err != nil {
+		panic(err)
+	}
 
-// 	w.Write(userJSON)
-// }
+	w.Write(userJSON)
+}
 
-// // /user/{id}/score/{score}
-// func postUser(w http.ResponseWriter, r *http.Request) {
+// /user/{id}/score/{score}
+func postUser(w http.ResponseWriter, r *http.Request) {
 
-// 	params := mux.Vars(r)
-// 	userid := params["id"]
-// 	score := params["score"]
+	params := mux.Vars(r)
+	userid := params["id"]
+	score := params["score"]
 
-// 	// this does not update any data source. This is scaffolding
-// 	w.Write([]byte("posted user " + userid + " with score " + score))
-// }
+	// this does not update any data source. This is scaffolding
+	w.Write([]byte("posted user " + userid + " with score " + score))
+}
 
-// // /user/{id}
-// func deleteUser(w http.ResponseWriter, r *http.Request) {
+// /user/{id}
+func deleteUser(w http.ResponseWriter, r *http.Request) {
 
-// 	params := mux.Vars(r)
-// 	userid := params["id"]
+	params := mux.Vars(r)
+	userid := params["id"]
 
-// 	// this does not update any data source. This is scaffolding
-// 	w.Write([]byte("deleted " + userid))
-// }
+	// this does not update any data source. This is scaffolding
+	w.Write([]byte("deleted " + userid))
+}
